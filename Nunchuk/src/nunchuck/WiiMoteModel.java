c55396571975208e86750cdf54ebfb38383b6b80
Mauro de Lyon <maurodelyon@gmail.com>
2015-04-30 18:28:46 +0200

    V4

--- Nunchuk/src/nunchuck/WiiMoteModel.java
+++ Nunchuk/src/nunchuck/WiiMoteModel.java
@@ -6,18 +6,33 @@ import java.awt.geom.Point2D;
 
 import javax.swing.Timer;
 
-import wiiusej.values.RawAcceleration;
-
 public class WiiMoteModel implements ActionListener {
 
 	private WiiMoteView view;
-	
+	public Point2D joyStick = new Point2D.Double(0, 0);
+	private float angle;
+	private float magnitude;
+
 	public WiiMoteModel(WiiMoteView view) {
 		this.view = view;
-		new Timer(200, this).start();
+		new Timer(1, this).start();
 	}
 
 	public void actionPerformed(ActionEvent e) {
+		// X
+		//System.out.println(Math.sin(Math.toRadians(angle)));
+		// Y
+		//System.out.println(-1*Math.cos(Math.toRadians(angle)));
 		
+		view.stick = new Point2D.Double(Math.sin(Math.toRadians(angle))*(magnitude * 250),(-1*Math.cos(Math.toRadians(angle))*(magnitude*250)));
+		view.repaint();
+	}
+
+	public void setAngle(float angle) {
+		this.angle = angle;
+	}
+
+	public void setMagnitude(float magnitude) {
+		this.magnitude = magnitude;
 	}
 }
